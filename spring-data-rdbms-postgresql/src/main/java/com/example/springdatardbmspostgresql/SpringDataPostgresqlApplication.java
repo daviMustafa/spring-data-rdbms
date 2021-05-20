6a8b082e2f0006fdc33c44ebbf542f2029f3918b
Davi Oliveira <davi.oliveira@pst.asseco.com>
2021-05-20 15:15:09 +0100

    Initial Commit

---
+++ spring-data-rdbms-postgresql/src/main/java/com/example/springdatardbmspostgresql/SpringDataPostgresqlApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdatardbmspostgresql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataPostgresqlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataPostgresqlApplication.class, args);
+    }
+
+}
---
+++ spring-data-rdbms-sqlserver/src/main/java/com/example/springdatardbmssqlserver/SpringDataSqlserverApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdatardbmssqlserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataSqlserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDataSqlserverApplication.class, args);
+    }
+
+}
